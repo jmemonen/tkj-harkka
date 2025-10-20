96afc5169e729d6cb927168c7e1f2160ce849f7a
mikko <mmnonov@gmail.com>
2025-10-20 18:54:11 +0300

    cleanup. tusb and the usb-serial thing working

--- src/main.c
+++ src/main.c
@@ -7,52 +7,79 @@
 #include <FreeRTOS.h>
 #include <queue.h>
 #include <task.h>
+#include <tusb.h>
 
 #include "tkjhat/sdk.h"
+#include "usbSerialDebug/helper.h"
 
-// Default stack size for the tasks. It can be reduced to 1024 if task is not using lot of memory.
-#define DEFAULT_STACK_SIZE 2048 
+// Default stack size for the tasks. It can be reduced to 1024 if task is not
+// using lot of memory.
+#define DEFAULT_STACK_SIZE 2048
+#define CDC_ITF_TX 1
 
-//Add here necessary states
-enum state { IDLE=1 };
+// Activates the TinyUSB library.
+static void usbTask(void *arg) {
+  (void)arg;
+  while (1) {
+    tud_task(); // With FreeRTOS wait for events
+                // Do not add vTaskDelay.
+  }
+}
+
+// Add here necessary states
+enum state { IDLE = 1 };
 enum state programState = IDLE;
 
-static void example_task(void *arg){
-    (void)arg;
+static void example_task(void *arg) {
+  (void)arg;
 
-    for(;;){
-        tight_loop_contents(); // Modify with application code here.
-        vTaskDelay(pdMS_TO_TICKS(2000));
-    }
+  for (;;) {
+    tight_loop_contents(); // Modify with application code here.
+    usb_serial_print("TESTI");
+    tud_cdc_n_write(CDC_ITF_TX, (uint_fast8_t const *)"TESTI\n", 7);
+    tud_cdc_n_write_flush(CDC_ITF_TX);
+    vTaskDelay(pdMS_TO_TICKS(2000));
+  }
 }
 
 int main() {
-    stdio_init_all();
-    // Uncomment this lines if you want to wait till the serial monitor is connected
-    /*while (!stdio_usb_connected()){
-        sleep_ms(10);
-    }*/ 
-    init_hat_sdk();
-    sleep_ms(300); //Wait some time so initialization of USB and hat is done.
+  // stdio_init_all();
+  // Uncomment this lines if you want to wait till the serial monitor is
+  // connected
+  /*while (!stdio_usb_connected()){
+      sleep_ms(10);
+  }*/
+  init_hat_sdk();
+  sleep_ms(300); // Wait some time so initialization of USB and hat is done.
 
-    TaskHandle_t myExampleTask = NULL;
-    // Create the tasks with xTaskCreate
-    BaseType_t result = xTaskCreate(example_task,       // (en) Task function
-                "example",              // (en) Name of the task 
-                DEFAULT_STACK_SIZE, // (en) Size of the stack for this task (in words). Generally 1024 or 2048
-                NULL,               // (en) Arguments of the task 
-                2,                  // (en) Priority of this task
-                &myExampleTask);    // (en) A handle to control the execution of this task
+  TaskHandle_t myExampleTask, hUSB = NULL;
 
-    if(result != pdPASS) {
-        printf("Example Task creation failed\n");
-        return 0;
-    }
+  xTaskCreate(usbTask, "usb", 2048, NULL, 3, &hUSB);
+#if (configNUMBER_OF_CORES > 1)
+  vTaskCoreAffinitySet(hUSB, 1u << 0);
+#endif
 
-    // Start the scheduler (never returns)
-    vTaskStartScheduler();
+  // Create the tasks with xTaskCreate
+  BaseType_t result = xTaskCreate(
+      example_task,       // (en) Task function
+      "example",          // (en) Name of the task
+      DEFAULT_STACK_SIZE, // (en) Size of the stack for this task (in words).
+                          // Generally 1024 or 2048
+      NULL,               // (en) Arguments of the task
+      2,                  // (en) Priority of this task
+      &myExampleTask);    // (en) A handle to control the execution of this task
 
-    // Never reach this line.
+  if (result != pdPASS) {
+    printf("Example Task creation failed\n");
     return 0;
-}
+  }
 
+  tusb_init();
+  usb_serial_init();
+
+  // Start the scheduler (never returns)
+  vTaskStartScheduler();
+
+  // Never reach this line.
+  return 0;
+}
