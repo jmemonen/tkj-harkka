f54d8a6e9ecbd275f18c289e383d8b7f0416e0d0
mikko <mmnonov@gmail.com>
2025-10-21 11:58:25 +0300

    reverted to using stdio to make flashing easier

--- src/main.c
+++ src/main.c
@@ -8,19 +8,20 @@
 #include <FreeRTOS.h>
 #include <queue.h>
 #include <task.h>
-#include <tusb.h>
+// #include <tusb.h>
 
 #include "portmacro.h"
-#include "tkjhat/sdk.h"
-#include "usbSerialDebug/helper.h"
 #include "sensors/sensors.h"
+#include "tkjhat/sdk.h"
+// #include "usbSerialDebug/helper.h"
 
 // Default stack size for the tasks. It can be reduced to 1024 if task is not
 // using lot of memory.
 #define DEFAULT_STACK_SIZE 2048
 #define CDC_ITF_TX 1
 
-// Activates the TinyUSB library.
+// Activates the TinyUSB library. Deactivated for easier flashing...
+/*
 static void usbTask(void *arg) {
   (void)arg;
   while (1) {
@@ -28,6 +29,7 @@ static void usbTask(void *arg) {
                 // Do not add vTaskDelay.
   }
 }
+*/
 
 // Reads the sensors.
 static void sensorTask(void *arg) {
@@ -36,7 +38,8 @@ static void sensorTask(void *arg) {
     int sensorReading = hello_sensors();
     char s[6];
     snprintf(s, 6, "%d\n", sensorReading);
-    usb_serial_print(s);
+    // usb_serial_print(s);
+    printf("Sensor task: %s", s);
     vTaskDelay(pdMS_TO_TICKS(1000));
   }
 }
@@ -49,16 +52,17 @@ static void example_task(void *arg) {
   (void)arg;
 
   for (;;) {
-    tight_loop_contents(); // Modify with application code here.
-    usb_serial_print("TESTI");
-    tud_cdc_n_write(CDC_ITF_TX, (uint_fast8_t const *)"TESTI\n", 7);
-    tud_cdc_n_write_flush(CDC_ITF_TX);
+    // tight_loop_contents(); // Modify with application code here.
+    // usb_serial_print("TESTI");
+    // tud_cdc_n_write(CDC_ITF_TX, (uint_fast8_t const *)"TESTI\n", 7);
+    // tud_cdc_n_write_flush(CDC_ITF_TX);
+    printf("Hello from example task!\n");
     vTaskDelay(pdMS_TO_TICKS(2000));
   }
 }
 
 int main() {
-  // stdio_init_all();
+  stdio_init_all();
   // Uncomment this lines if you want to wait till the serial monitor is
   // connected
   /*while (!stdio_usb_connected()){
@@ -67,12 +71,15 @@ int main() {
   init_hat_sdk();
   sleep_ms(300); // Wait some time so initialization of USB and hat is done.
 
-  TaskHandle_t myExampleTask, hUSB, sensor = NULL;
+  TaskHandle_t myExampleTask, sensor = NULL;
+  // TaskHandle_t hUSB = NULLM
 
+  /*
   xTaskCreate(usbTask, "usb", 2048, NULL, 3, &hUSB);
 #if (configNUMBER_OF_CORES > 1)
   vTaskCoreAffinitySet(hUSB, 1u << 0);
 #endif
+*/
 
   // Create the tasks with xTaskCreate
   BaseType_t result = xTaskCreate(
@@ -85,14 +92,15 @@ int main() {
       &myExampleTask);    // (en) A handle to control the execution of this task
 
   if (result != pdPASS) {
-    usb_serial_print("Example Task creation failed\n");
+    printf("Example Task creation failed\n");
     return 0;
   }
 
-  result = xTaskCreate(sensorTask, "sensor", DEFAULT_STACK_SIZE, NULL, 2, &sensor);
+  result =
+      xTaskCreate(sensorTask, "sensor", DEFAULT_STACK_SIZE, NULL, 2, &sensor);
 
-  tusb_init();
-  usb_serial_init();
+  // tusb_init();
+  // usb_serial_init();
 
   // Start the scheduler (never returns)
   vTaskStartScheduler();
