5e4889ab0ef5facfe57f76635a9afd169dee6bbe
mikko <mmnonov@gmail.com>
2025-11-01 23:35:01 +0200

    text to morse working

--- libs/morso/tests/morso_test.c
+++ libs/morso/tests/morso_test.c
@@ -4,10 +4,12 @@
 
 void test_char_to_morse(void);
 void test_morse_to_char(void);
+void test_encode_morse_msg(void);
 
 int main(void) {
   test_char_to_morse();
   test_morse_to_char();
+  test_encode_morse_msg();
   return 0;
 }
 
@@ -47,4 +49,45 @@ void test_morse_to_char() {
     char c_result = morse_to_char(str);
     printf("%c\t:\t%s\n", c, str);
   }
+}
+
+void test_encode_morse_msg(void) {
+  const size_t BUF_SIZE = 128;
+  char buf[BUF_SIZE];
+
+  char *str = "abcdefg hijklmnop qrstuvxyz";
+  printf("\nEncoding message: %s :\t", str);
+  int res = encode_morse_msg(str, buf, BUF_SIZE);
+  printf("%s\n", buf);
+  printf("return code %d\n", res);
+
+  buf[0] = '\0';
+  str = "1234";
+  printf("\nEncoding message: %s :\t", str);
+  res = encode_morse_msg(str, buf, BUF_SIZE);
+  printf("%s\n", buf);
+  printf("return code %d\n", res);
+
+  buf[0] = '\0';
+  str = "1234";
+  printf("\nEncoding message: %s :\t", str);
+  res = encode_morse_msg(str, buf, BUF_SIZE);
+  printf("%s\n", buf);
+  printf("return code %d\n", res);
+
+  buf[0] = '\0';
+  str = "1234";
+  printf("\nEncoding message: %s :\t", str);
+  res = encode_morse_msg(NULL, buf, BUF_SIZE);
+  printf("%s\n", buf);
+  printf("return code %d\n", res);
+
+  // Edge case where \0 should come immediately after a symbol.
+  buf[0] = '\0';
+  char lil_buf[2];
+  str = "e";
+  printf("\nEncoding message: %s :\t", str);
+  res = encode_morse_msg(str, lil_buf, 2);
+  printf("%s\n", lil_buf);
+  printf("return code %d\n", res);
 }
